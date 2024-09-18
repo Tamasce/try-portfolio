1e00937855af44330fc60c99b1eb077266071a28
Tamasce <giovannistancampiano1999@gmail.com>
2024-09-18 17:35:21 +0200

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "giovannistancampiano1999@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
